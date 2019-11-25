792162e4c3625b281360e7151357365447720a2d
logan-lieou <logan.lieou@gmail.com>
2019-11-25 14:55:55 -0600

    app

--- Application.py
+++ Application.py
@@ -1,6 +1,11 @@
 from tkinter import *
 
 root = Tk()
-canvas = Canvas(root, width = 300, height = 300)
+canvas = Canvas(root, width = 300, height = 300, bg = "yellow")
 canvas.pack()
+
+button = Button(root)
+button.pack()
+
 mainloop.root()
+
