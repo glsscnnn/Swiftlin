319c903255b498a403b8c38255a2521e57d07bef
logan-lieou <logan.lieou@gmail.com>
2019-11-26 10:23:41 -0600

    mild changes

--- complier/main/src/main.java
+++ complier/main/src/main.java
@@ -1,3 +1,4 @@
+import org.antlr.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -6,13 +7,13 @@ import java.io.IOException;
 
 public class main {
     public static void main(String[] args) throws IOException{
+        CharStream charStream = CharStreams.fromFileName("./test.cpp");
+        CPP14Lexer cpp14Lexer = new CPP14Lexer(charStream);
+        CommonTokenStream commonTokenStream = new CommonTokenStream(cpp14Lexer);
+        CPP14Parser cpp14Parser = new CPP14Parser(commonTokenStream);
 
-        CharStream charStream = CharStreams.fromFileName("./test.swift");
-        Swift3Lexer swift3Lexer = new Swift3Lexer(charStream);
-        CommonTokenStream commonTokenStream = new CommonTokenStream(swift3Lexer);
-        Swift3Parser swift3Parser = new Swift3Parser(commonTokenStream);
+        CPP14Parser.TranslationunitContext parseTree = cpp14Parser.translationunit();
 
-        swift3Parser.control_transfer_statement();
 
         System.out.println("Done");
     }
